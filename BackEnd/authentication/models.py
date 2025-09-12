0bbddb8abefd1998ed2f9227b15f1278466ca225
Daemon3T <tokyoken24@gmail.com>
2025-09-12 14:42:09 +0800

    Update The Requirements.txt

--- BackEnd/authentication/models.py
+++ BackEnd/authentication/models.py
@@ -56,28 +56,6 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.get_full_name()} ({self.get_user_type_display()})"
 
-# class Province(models.Model):
-#     name = models.CharField(max_length=100)
-    
-#     def __str__(self):
-#         return self.name
-
-# class CityMunicipality(models.Model):
-#     name = models.CharField(max_length=100)
-#     zip_code = models.CharField(max_length=10, blank=True)
-#     province = models.ForeignKey(Province, on_delete=models.CASCADE)
-    
-#     def __str__(self):
-#         return self.name
-
-# class Address(models.Model):
-#     street = models.CharField(max_length=200)
-#     barangay = models.CharField(max_length=100)
-#     city_municipality = models.ForeignKey(CityMunicipality, on_delete=models.CASCADE)
-    
-#     def __str__(self):
-#         return f"{self.street}, {self.barangay}, {self.city_municipality.name}"
-
 class OTPVerification(models.Model):
     email = models.EmailField()
     otp_code = models.CharField(max_length=6)
